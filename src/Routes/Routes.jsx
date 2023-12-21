20d68fe654bc4e809b6897bd423e3d24fba04ef1
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-12-21 16:31:20 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const myCreatedRoutes= createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello World</div>
+    }
+])
+export default myCreatedRoutes;
